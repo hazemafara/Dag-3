cfa7f7b563c7b8ce3fc6f9a4edc7f88121316355
hazemafara <94962699+hazemafara@users.noreply.github.com>
2024-06-27 16:18:39 +0200

    models merge

---
+++ app/Models/AllergyPerPerson.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AllergyPerPerson extends Model
+{
+    protected $table = 'allergy_per_person';
+        use HasFactory;
+}
