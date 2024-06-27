2d1ad2cfcad04808b80f42a776ac6d95685e04d2
hazemafara <94962699+hazemafara@users.noreply.github.com>
2024-06-27 16:20:45 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AllergyController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+
+Route::get('/allergies', [AllergyController::class, 'index'])->name('allergies.index');
+Route::get('/allergies', [AllergyController::class, 'filterByCategory'])->name('allergies.index');
+Route::get('/family/{id}/detail', [AllergyController::class, 'allergyDetails'])->name('family.detail');
+
+Route::get('/allergies/{id}/edit', [AllergyController::class, 'edit'])->name('allergy.edit');
+Route::put('/allergies/{id}', [AllergyController::class, 'update'])->name('allergy.update');
